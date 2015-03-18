0e069aa0bdcc7dde719961853c9106b5f00328fb
Camille Kander <camille@sproutling.com>
2015-03-18 09:36:40 -0700

    Initial commit

---
+++ OnboardingProgressIndicator/STOnboardingProgressView.swift
@@ -0,0 +1,13 @@
+//
+//  STOnboardingProgressView.swift
+//  OnboardingProgressIndicator
+//
+//  Created by Camille Kander on 3/18/15.
+//  Copyright (c) 2015 Sproutling. All rights reserved.
+//
+
+import UIKit
+
+class STOnboardingProgressView: UIView {
+
+}
