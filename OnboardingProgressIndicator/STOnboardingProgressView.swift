4fec42d770ff4cbc38b8e897c1a5506fac3d1385
Camille Kander <camille@sproutling.com>
2015-03-18 11:19:14 -0700

    First version

--- OnboardingProgressIndicator/STOnboardingProgressView.swift
+++ OnboardingProgressIndicator/STOnboardingProgressView.swift
@@ -8,6 +8,54 @@
 
 import UIKit
 
+let kColorComplete      = UIColor(red: 0xF3/0xFF, green: 0xAB/0xFF, blue: 0x36/0xFF, alpha: 1.0) // #F3AB36
+let kColorIncomplete    = UIColor(white: 0xE3/0xFF, alpha: 1.0) // #E3E3E3
+let kSeparatorWidth     = CGFloat(2.0)
+
 class STOnboardingProgressView: UIView {
+    
+    let numberOfSteps: UInt = 5
+    var progress: UInt = 0 {
+        didSet {
+            if (progress > numberOfSteps) {
+                progress = numberOfSteps
+            }
+            
+            for view in self.stepViews {
+                let index = find(stepViews, view)
+                if (index < Int(progress)) {
+                    view.backgroundColor = kColorComplete
+                } else {
+                    view.backgroundColor = kColorIncomplete
+                }
+            }
+        }
+    }
+    private var stepViews: [UIView]
+    
+    override init(frame: CGRect) {
+        
+        // create the subviews
+        stepViews = []
+        for var i = UInt(0); i < numberOfSteps; i++ {
+            stepViews.append(UIView());
+        }
+                
+        super.init(frame: frame)
+        
+        // set the frames for the subviews
+        for view in stepViews {
+            let index = find(stepViews, view)!
+            let width = (frame.width - (kSeparatorWidth * CGFloat(numberOfSteps - 1))) / CGFloat(numberOfSteps)
+            let x = (width + kSeparatorWidth) * CGFloat(index)
+            view.frame = CGRectMake(x, 0, width, frame.size.height)
+            view.backgroundColor = kColorIncomplete
+            self.addSubview(view)
+        }
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
--- OnboardingProgressIndicator/ViewController.swift
+++ OnboardingProgressIndicator/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    var progressView = STOnboardingProgressView(frame: CGRectMake(20, 50, 335, 20))
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+//        self.progressView = STOnboardingProgressView(frame: CGRectMake(20, 50, 335, 20))
+        self.progressView.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(progressView)
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func set0(sender: UIButton) {
+        self.progressView.progress = 0
+    }
 
+    @IBAction func set1(sender: UIButton) {
+        self.progressView.progress = 1
+    }
+    
+    @IBAction func set2(sender: UIButton) {
+        self.progressView.progress = 2
+    }
+    
+    @IBAction func set3(sender: UIButton) {
+        self.progressView.progress = 3
+    }
+    
+    @IBAction func set4(sender: UIButton) {
+        self.progressView.progress = 4
+    }
+    
+    @IBAction func set5(sender: UIButton) {
+        self.progressView.progress = 5
+    }
 }
 
